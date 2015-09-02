291cf6431181b36f61eace0839913eec86ba2085
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2015-09-02 15:20:46 +1000

    changed to exec

--- manifests/remote_answers.pp
+++ manifests/remote_answers.pp
@@ -1,13 +1,8 @@
 class classroom_addons::remote_answers (
   $master_vm_name,
 ) {
-  vcsrepo { $classroom::params::workdir :
-    ensure   => present,
-    provider => 'git',
-    remote   => origin,
-    source   => {
-      'origin'  => "${classroom::params::machine_name}@master.puppetlabs.vm:/var/repositories/${classroom::params::machine_name}.git",
-      'answers' => "${master_vm_name}@master.puppetlabs.vm:/var/repositories/${master_vm_name}.git"
-    }
+  exec { 'git remote add answers gandalf@master.puppetlabs.vm:/var/repositories/gandalf.git':
+    path   => $::path,
+    unless => 'git remove -v | grep gandalf',
   }
 }
